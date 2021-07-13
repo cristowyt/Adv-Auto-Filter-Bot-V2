ed2e0963a0a738ea8cd1d26612ec22c3f4882bdf
cristowyt <86834940+cristowyt@users.noreply.github.com>
2021-07-13 11:13:48 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,12 +5,12 @@
 class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+I Am a auto filter bot.You can't use me to your group😁.I am belongs to @Televisionmarket🐽🐽
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@cristow666
 </i>"""    
     
     HELP_TEXT = """
@@ -43,16 +43,16 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@cristow
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/cristow666">Cristow</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/cristowyt/Adv-Auto-Filter-Bot-V2">Click Me</a></i>
 """
